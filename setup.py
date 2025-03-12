376af5f2875ea43eec3b1f080d74016c4b8df7a6
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-03-12 10:09:25 +0900

    setup.py を追加

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mofeapi",
+    version="0.1.0",
+    author="Ryohei Kobayashi",
+    author_email="kobaryo222912@gmail.com",
+    description="Unofficial Python wrapper for MOFE API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ryohei222/mofeapi-python",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
